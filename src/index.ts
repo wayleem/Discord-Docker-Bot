5a161967a2a700beebdb2e348056fc5107ebdeb8
Will H <wayleemh@gmail.com>
2024-01-26 21:09:54 -0500

    change ssh to deploy on server

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 import { Client, GatewayIntentBits, PermissionFlagsBits, Message } from 'discord.js'
-import { NodeSSH } from 'node-ssh'
+import { exec } from 'child_process'
 import * as dotenv from 'dotenv'
-import * as fs from 'fs'
-import * as path from 'path'
-import axios from 'axios'
-import { SSHConfig, Configs } from './types'
-
 
 dotenv.config()
 
 const bot = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 })
+
+bot.once('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`)
+});
 
 bot.on('messageCreate', async message => {
   if (!message.guild || !message.member?.permissions.has(PermissionFlagsBits.Administrator)) {
@@ -23,114 +22,41 @@ bot.on('messageCreate', async message => {
   console.log(message.content)
   console.log(args)
 
-  if (message.content.startsWith('!sethost')) {
-    setHost(message, args)
-  } else if (message.content.startsWith('!setusername')) {
-    setUsername(message, args)
-  } else if (message.content.startsWith('!setport')) {
-    setPort(message, args)
-  } else if (message.content.startsWith('!setkey')) {
-    setKey(message)
-  } else if (message.content === '!viewconfig') {
-    viewConfig(message)
+  if (message.content === "!start") {
+    startServer(message)
+  } else if (message.content === "!close") {
+    closeServer(message)
   }
 
 })
 
-function setHost(message: Message, args: string[]) {
-  if (args.length !== 1) {
-    return message.reply('Please provide the host IP. Usage: `!sethost [host]`')
-  }
-  updateConfig(message.guild.id, 'host', args[0])
-  message.reply('Host updated successfully.')
+function startServer(message: Message) {
+    exec('docker compose up', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return message.reply(`Error executing docker compose up: ${error.message}`);
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return message.reply(`docker compose up executed with errors: \`${stderr}\``);
+        }
+        message.reply(`docker compose up executed successfully: \`${stdout}\``);
+    });
 }
 
-function setUsername(message: Message, args: string[]) {
-  if (args.length !== 1) {
-    return message.reply('Please provide the username. Usage: `!setusername [username]`')
-  }
-  updateConfig(message.guild.id, 'username', args[0])
-  message.reply('Username updated successfully.')
+function closeServer(message: Message) {
+    exec('docker compose up', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return message.reply(`Error executing docker compose up: ${error.message}`);
+        }
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+            return message.reply(`docker compose up executed with errors: \`${stderr}\``);
+        }
+        message.reply(`docker compose up executed successfully: \`${stdout}\``);
+    });
 }
 
-function setPort(message: Message, args: string[]) {
-  if (args.length !== 1) {
-    return message.reply('Please provide the username. Usage: `!setport [port]`')
-  }
-  updateConfig(message.guild.id, 'port', args[0])
-  message.reply('Port updated successfully.')
-}
-
-async function setKey(message: Message<boolean>) {
-  if (message.attachments.size > 0) {
-    const file = message.attachments.first() // Get the first attachment
-    if (file) {
-      try {
-        // Download the file, read its contents, and store the key
-        const response = await axios.get(file.url, { responseType: 'text' })
-        const privateKey = response.data
-
-        updateConfig(message.guild!.id, 'privateKey', privateKey)
-        message.reply('SSH private key updated successfully.')
-      } catch (error) {
-        console.error("Error setting SSH key:", error)
-        message.reply('Failed to set SSH private key. Please try again.')
-      }
-    }
-  } else {
-    message.reply('Please attach a file containing the SSH private key.')
-  }
-}
-
-function viewConfig(message: Message<boolean>) {
-  const guildId = message.guild?.id;
-  if (!guildId) return;
-
-  const configFilePath = path.join(__dirname, 'sshConfigs.json');
-  if (!fs.existsSync(configFilePath)) {
-    return message.reply('No configuration found.');
-  }
-
-  const configs: Configs = JSON.parse(fs.readFileSync(configFilePath, 'utf-8'));
-  const config = configs[guildId];
-
-  if (!config) {
-    message.reply('No configuration set for this server.');
-  } else {
-    message.reply(`Current SSH Configuration:\nHost: ${config.host || 'Not set'}\nUsername: ${config.username || 'Not set'}\nPort: ${config.port || 'Not set'}`);
-  }
-}
-
-function updateConfig(guildId: string, key: keyof SSHConfig, value: string) {
-  const configFilePath = path.join(__dirname, 'sshConfigs.json')
-  let configs: Configs = {}
-
-  if (fs.existsSync(configFilePath)) {
-    configs = JSON.parse(fs.readFileSync(configFilePath, 'utf-8'))
-  }
-
-  const guildConfig = configs[guildId] || {
-    host: '',
-    username: '',
-    port: '22', // Default SSH port, change if needed
-    privateKey: ''
-  }
-  guildConfig[key] = value
-  configs[guildId] = guildConfig
-
-  fs.writeFileSync(configFilePath, JSON.stringify(configs, null, 2))
-}
-
-function saveSSHConfig(guildId: string, config: SSHConfig) {
-  const configFilePath = path.join(__dirname, 'sshConfigs.json')
-  let configs: Configs = {}
-
-  if (fs.existsSync(configFilePath)) {
-    configs = JSON.parse(fs.readFileSync(configFilePath, 'utf-8'))
-  }
-
-  configs[guildId] = config
-  fs.writeFileSync(configFilePath, JSON.stringify(configs, null, 2))
-}
 
 bot.login(process.env.DISCORD_TOKEN)
